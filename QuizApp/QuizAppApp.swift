ccfac4a3de451d60d108a36511d4754a23763a55
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-06-10 17:49:06 -0400

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Nicholas Gramenos on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
